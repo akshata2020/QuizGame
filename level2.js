f6690162d2f494029e1244e174219dc2105ac78f
akshata2020 <82460094+akshata2020@users.noreply.github.com>
2021-04-14 11:36:08 +0530

    Add files via upload

---
+++ level2.js
@@ -0,0 +1,61 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "When clock frequency goes up in ARM microprocessor, dynamic power ___?",
+    answer: "Increases",
+    options: [
+      "Increases",
+      "Decreases",
+      "Becomes zero",
+      "Doesn't change"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What type of bit in the ARM memory mimics to that of the protection unit of ARM management unit?",
+    answer: "cacheable bit",
+    options: [
+      "Permission bit",
+      "cacheable bit",
+      "Buffer bit",
+      "Access permission bit"
+    ]
+  },
+    {
+    numb: 3,
+    question: "The additional duplicate register used in ARM machines are called as ___",
+    answer: "Banked registers",
+    options: [
+      "Copied-registers",
+      "Extra registers",
+      "Banked registers",
+      "Extremital registers"
+    ]
+  },
+    {
+    numb: 4,
+    question: "RISC processor operates on data held in ",
+    answer: "Both memory and registers",
+    options: [
+      "Memory",
+      "Register",
+      "Both memory and registers",
+      "None of the above"
+    ]
+  },
+   
+  
+
+    {
+    numb: 5,
+    question: "ARM processor data items are placed in __ file.",
+    answer: "Register",
+    options: [
+      "I/O",
+      "Register",
+      "Memory",
+      "All of the above"
+    ]
+  },
+];
---
+++ mcq.js
@@ -0,0 +1,129 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+  numb: 1,
+  question: "The ARM core uses __ Architecture.",
+  answer: "RISC",
+  options: [
+    "CISC",
+    "RISC",
+    "Both",
+    "None of the above"
+  ]
+},
+  {
+  numb: 2,
+  question: "How many registers are there in ARM7?",
+  answer: "37 registers(31 GPR and 6 SPR)",
+  options: [
+    "35 register( 28 GPR and 7 SPR)",
+    "37 registers(28 GPR and 9 SPR)",
+    "37 registers(31 GPR and 6 SPR)",
+    "35 register(30 GPR and 5 SPR"
+  ]
+},
+  {
+  numb: 3,
+  question: "What does RISC stand for?",
+  answer: "Reduced instruction set",
+  options: [
+    "Run instruction set",
+    "Return instruction set",
+    "Reduced instruction set",
+    "None of the above"
+  ]
+},
+  {
+  numb: 4,
+  question: "What are the significant designing factors taken into consideration for RISC processors?",
+  answer: "All of the above",
+  options: [
+    "Simplicity in instruction set",
+    "Pipeline instruction optimization",
+    "Register usage optimization",
+    "All of the above"
+  ]
+},
+  {
+  numb: 5,
+  question: "Run time mapping from virtual to physical address is done by __",
+  answer: "Memory Management Unit",
+  options: [
+    "Memory Management Unit",
+    "CPU",
+    "PCI",
+    "None of the above"
+  ]
+},
+
+  {
+  numb: 6,
+  question: "How many regions created by the memory range in the ARM architecture?",
+  answer: "8",
+  options: [
+    "4",
+    "8",
+    "16",
+    "32"
+  ]
+},
+  {
+  numb: 7,
+  question: "Which of the following uses a priority level for permitting data?",
+  answer: "ARM protection memory management unit",
+  options: [
+    "ARM protection memory management unit",
+    "ARM memory management unit",
+    "Bus interface unit",
+    "Execution unit"
+  ]
+},
+{
+  numb: 8,
+  question: "Which of the following is true?",
+  answer: "Risc Focus on software",
+  options: [
+    "The RISC processor has a more complicated design than CISC",
+    "Risc Focus on software",
+    "Cisc Focus on software",
+    "Risc has Variable sized instructions"
+  ]
+},
+{
+  numb: 9,
+  question: "Which of the following uses a priority level for permitting data?",
+  answer: "ARM protection memory management unit",
+  options: [
+    "ARM protection memory management unit",
+    "ARM memory management unit",
+    "Bus interface unit",
+    "Execution unit"
+  ]
+},
+
+{
+  numb: 10,
+  question: "Which of the following includes a tripped down memory management unit?",
+  answer: "memory protection unit",
+  options: [
+    "memory protection unit",
+    "memory real mode",
+    "memory management unit",
+    "bus interface unit"
+  ]
+},
+
+
+
+//   {
+//   numb: 6,
+//   question: "Your Question is Here",
+//   answer: "Correct answer of the question is here",
+//   options: [
+//     "Option 1",
+//     "option 2",
+//     "option 3",
+//     "option 4"
+//   ]
+// },
+];
